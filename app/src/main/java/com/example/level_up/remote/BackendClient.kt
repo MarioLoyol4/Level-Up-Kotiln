7213817d8cc1b3b223a927a3901c6b01ee4bd151
MarioLoyol4 <mar.loyolac@duocuc.cl>
2025-12-19 00:21:53 -0300

    Todo listo para maniana

--- app/src/main/java/com/example/level_up/remote/BackendClient.kt
+++ app/src/main/java/com/example/level_up/remote/BackendClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object BackendClient {
-    private const val BASE_URL = "http://98.94.229.120:9090"
+    private const val BASE_URL = "http://10.0.2.2:9090"
 
     val api: BackendApi by lazy {
         Retrofit.Builder()
